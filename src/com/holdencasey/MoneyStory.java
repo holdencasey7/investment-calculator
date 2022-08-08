304673dbc368f158a2124ef6996a6897da4eec97
Holden Casey <me@holdencasey.com>
2022-08-08 00:05:47 -0400

    Added MoneyStory class

---
+++ src/com/holdencasey/MoneyStory.java
@@ -0,0 +1,12 @@
+package com.holdencasey;
+
+/**
+ * The final value and story of the money values.
+ */
+public class MoneyStory {
+    final Profile investmentProfile; //the investment profile
+
+    MoneyStory(Profile investmentProfile) {
+        this.investmentProfile = investmentProfile;
+    }
+}
