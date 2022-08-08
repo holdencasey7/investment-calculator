fac1f2afff82ee879759efefdd762d974d7de151
Holden Casey <me@holdencasey.com>
2022-08-08 00:23:10 -0400

    Created end value calculator method signatures in MoneyStory class

--- src/com/holdencasey/MoneyStory.java
+++ src/com/holdencasey/MoneyStory.java
@@ -6,7 +6,31 @@ package com.holdencasey;
 public class MoneyStory {
     final Profile investmentProfile; //the investment profile
 
+    /**
+     * Main constructor.
+     *
+     * @param investmentProfile the investment profile.
+     */
     MoneyStory(Profile investmentProfile) {
         this.investmentProfile = investmentProfile;
+    }
+
+    /**
+     * Calculates total investment value after all contributions and returns.
+     *
+     * @return the final investment value, in dollars.
+     */
+    float calculateEndValue() {
+        return 0f; //Placeholder
+    }
+
+    /**
+     * Static method that calls instance method using new object created from passed Profile.
+     *
+     * @param investmentProfile the Profile to use.
+     * @return the final investment value, in dollars.
+     */
+    static float calculateEndValue(Profile investmentProfile) {
+        return (new MoneyStory(investmentProfile)).calculateEndValue();
     }
 }
