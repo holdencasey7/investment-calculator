1a1c7be758725514bd3e177930fccc8c04e18939
Holden Casey <me@holdencasey.com>
2022-08-08 14:27:40 -0400

    Added testing case to Main

--- src/com/holdencasey/investmentcalculator/Main.java
+++ src/com/holdencasey/investmentcalculator/Main.java
@@ -1,4 +1,21 @@
 package com.holdencasey.investmentcalculator;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] lengths = {4, 2, 1, 3};
+        TimePeriod testTimePeriod = new TimePeriod(lengths);
+
+        float[] contributions = {10.5f, 3f, 9f, 11.3f};
+        Contribution testContribution = new Contribution(contributions, testTimePeriod);
+
+        float[] returns = {8f, 6.5f, 14f, 6f};
+        ReturnRate testReturnRate = new ReturnRate(returns, testTimePeriod);
+
+        Profile testProfile = new Profile(testContribution, testReturnRate);
+
+        MoneyStory testMoneyStory = new MoneyStory(testProfile);
+
+        System.out.println(testMoneyStory.makeContributionArray());
+    }
+
 }
