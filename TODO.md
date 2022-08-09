df78b8736decb2629161b591c5d791cffb6b0c9f
Holden Casey <me@holdencasey.com>
2022-08-09 00:05:51 -0400

    Wrote more realistic test including negative contributions (withdraw). Worked.

--- TODO.md
+++ TODO.md
@@ -4,8 +4,8 @@
 ~~Test Year[] array builder after adding toString()~~  
 ~~Add toString() to all other classes~~  
 Create methods for visualizing MoneyStory  
-Add total amount contributed  
-Test full process without user input  
+~~Add total amount contributed~~  
+~~Test full process without user input~~  
 Create system for user input  
 Add exceptions and handling  
 Test full process with user input  
--- src/com/holdencasey/investmentcalculator/Main.java
+++ src/com/holdencasey/investmentcalculator/Main.java
@@ -5,6 +5,27 @@ import java.math.RoundingMode;
 
 public class Main {
     public static void main(String[] args) {
+        /* New more realistic test */
+        int[] contLengths = {5, 40, 20};
+        BigDecimal[] contributions = {
+                new BigDecimal(500),
+                new BigDecimal(6000),
+                new BigDecimal(-10000)
+        };
+        Contribution testContribution = new Contribution(contributions, contLengths);
+
+        int[] returnLengths = {45, 20};
+        BigDecimal[] returns = {
+                new BigDecimal(9.5),
+                new BigDecimal(5.5)
+        };
+        ReturnRate testReturnRate = new ReturnRate(returns, returnLengths);
+
+        MoneyStory testMoneyStory = new MoneyStory(new Profile(testContribution, testReturnRate));
+
+        testMoneyStory.printStartAndEndWithContributions();
+
+        /* Old Test
         int[] lengths = {4, 2, 1, 3};
         TimePeriod testTimePeriod = new TimePeriod(lengths);
 
@@ -20,23 +41,23 @@ public class Main {
 
         MoneyStory testMoneyStory = new MoneyStory(testProfile);
 
-//        testMoneyStory.printYears();
-//        testMoneyStory.printStartAndEnd();
-//        testMoneyStory.printStartAndEndWithContributions();
-//        testMoneyStory.printContributions();
-//        System.out.println(testTimePeriod);
-//        System.out.println(testReturnRate);
-//        System.out.println(testContribution);
-//        System.out.println(testProfile);
-//        System.out.println(testMoneyStory);
-//        System.out.println(testMoneyStory.makeContributionArray());
-//        System.out.println(testMoneyStory.makeReturnArray());
-//
-//        Year[] testYearArray = testMoneyStory.makeYearArray();
-//
-//        for (Year year : testYearArray) {
-//            System.out.println(year);
-//        }
+        testMoneyStory.printYears();
+        testMoneyStory.printStartAndEnd();
+        testMoneyStory.printStartAndEndWithContributions();
+        testMoneyStory.printContributions();
+        System.out.println(testTimePeriod);
+        System.out.println(testReturnRate);
+        System.out.println(testContribution);
+        System.out.println(testProfile);
+        System.out.println(testMoneyStory);
+        System.out.println(testMoneyStory.makeContributionArray());
+        System.out.println(testMoneyStory.makeReturnArray());
+
+        Year[] testYearArray = testMoneyStory.makeYearArray();
+
+        for (Year year : testYearArray) {
+            System.out.println(year);
+        } */
 
     }
 
